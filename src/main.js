1c35c1463241139398236e4b0572af5a4c77bc57
barvo1 <804460982@qq.com>
2024-12-05 15:46:12 +0800

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import elementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import * as echarts from 'echarts' 
+
+createApp(App).mount('#app')
